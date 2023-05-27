a03665848b499c02af06da771a42da50249947ce
Renan Montano Paz <renanmontanopaz@outlook.com>
2023-05-27 15:04:18 -0300

    corrigindo erro

--- src/contexts/Toast/index.tsx
+++ src/contexts/Toast/index.tsx
@@ -1,46 +1,55 @@
-import React, {createContext, useState} from "react";
+import { createContext, useState } from "react"
+import { Toast } from "../../components/Toast";
 
 type ToastProviderProps = {
     children: React.ReactNode;
 }
+
 type Toast = {
     message: string;
-    type: 'success'|'danger';
+    type: 'success' | 'danger';
 }
+
 export type ToastContextProps = {
     isHidden: boolean;
     showToast(data: Toast): void;
 }
 
-
-export const ToastContext = createContext<ToastContextProps>({
+const ToastContext = createContext<ToastContextProps>({
     isHidden: false,
-    showToast: () => {}
+    showToast: () => { }
 } as ToastContextProps);
 
-const ToastProvider = ({children}: ToastProviderProps) =>{
-    const [isHidden, setIsHidden]= useState<boolean>(true);
-    const {Toast, setToast} = useState<Toast>({
-    message: '',
-    type: 'success'
-})
-    const showToast = ({message, type}: Toast) => {
+const ToastProvider = ({ children }: ToastProviderProps) => {
+
+    const [isHidden, setIsHidden] = useState<boolean>(true);
+    const [toast, setToast] = useState<Toast>({
+        message: '',
+        type: 'success'
+    })
+
+    const showToast = ({ message, type }: Toast) => {
         setToast({
             message,
             type
         })
+
         setIsHidden(false);
-        setTimeout(()=> {
-            setIsHidden(true)
+
+        setTimeout(() => {
+            setIsHidden(true);
         }, 2800);
     }
-    return(
-        <ToastContext.Provider value={{isHidden, showToast}}>
-            {isHidden && <Toast message={Toast.message} type={Toast.type}/>}
+
+
+    return (
+        <ToastContext.Provider value={{ isHidden, showToast }} >
+            {!isHidden && <Toast message={toast.message} type={toast.type} />}
             {children}
         </ToastContext.Provider>
     )
 }
 
-export {ToastProvider};
+export { ToastProvider };
+
 export default ToastContext;
